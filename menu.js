a4cf4d02cf2d3768ca39473c827c94b5af71a964
Andres Salgado <chato94@gmail.com>
2017-02-06 21:37:02 -0600

    Small, basic function necessary for menu function

--- menu.js
+++ menu.js
@@ -1,3 +1,10 @@
 function Menu () {
+	// credit: http://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+	function getElementOffset (element) {
+		var rect = element.getBoundingClientRect (),
+			x = window.scrollX? window.scrollX : document.documentElement.scrollLeft,
+			y = window.scrolllY? window.scrolllY : document.document.scrollTop;
 
+		return {left: rect.left + x, top: rect.top + y};
+	}
 }
