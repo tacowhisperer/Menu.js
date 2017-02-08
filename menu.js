16dad18b3795f42d934ef534c1244d979067a773
Andres Salgado <chato94@gmail.com>
2017-02-07 21:01:49 -0600

    Bug fixes

--- menu.js
+++ menu.js
@@ -1,9 +1,13 @@
 function Menu () {
+	var cssAnimator = new CSSAnimator ();
+
+	
+	
 	// credit: http://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
 	function getElementOffset (element) {
 		var rect = element.getBoundingClientRect (),
 			x = window.scrollX? window.scrollX : document.documentElement.scrollLeft,
-			y = window.scrolllY? window.scrolllY : document.document.scrollTop;
+			y = window.scrolllY? window.scrolllY : document.documentElement.scrollTop;
 
 		return {left: rect.left + x, top: rect.top + y};
 	}
